4c5c4964a5f8cd45f78dfa07249c2a39b1d5f751
Shanick <44624042+shanickcuello@users.noreply.github.com>
2025-05-07 17:45:14 +0200

    Update go_phish_launcher.sh

--- go_phish_launcher.sh
+++ go_phish_launcher.sh
@@ -1,18 +1,21 @@
 #remember to change permission of the file. 'chmod +x go_phish_launcher
 #and then launch the script 'sudo ./go_phish_launsher.sh email'
+green() {
+  echo -e "\033[0;32m$1\033[0m"
+}
 EMAIL="$1"
 
 # Validate email
 if [ -z "$EMAIL" ]; then
-  echo "Error: You need to run the script with an email."
-  echo "Example: sudo ./go_phish_launcher.sh email_email@gmail.com"
+  green "Error: You need to run the script with an email."
+  green "Example: sudo ./go_phish_launcher.sh email_email@gmail.com"
   exit 1
 fi
-echo "Starting..."
-echo "Installing go phish"
+green "Starting..."
+green "Installing go phish"
 cd ~ || exit
 mkdir "go_phish"
-echo "Created folder on $(pwd)"
+green "Created folder on $(pwd)"
 cd go_phish || exit
 wget https://github.com/gophish/gophish/releases/download/v0.12.1/gophish-v0.12.1-linux-64bit.zip
 sudo apt install unzip
@@ -21,7 +24,7 @@ sudo apt install openssl
 read -n1 -r -p "We will create a SSL certificate. The only required field is the email. You can click on 'enter' in the rest of fields" key
 openssl req -newkey rsa:2048 -nodes -keyout gophish.key -x509 -days 365 -out gophish.crt
 sudo apt install jq -y
-echo "Updating config.json..."
+green "Updating config.json..."
 CERT_PATH="$(pwd)/gophish.crt"
 KEY_PATH="$(pwd)/gophish.key"
 jq --arg cert "$CERT_PATH" \
@@ -34,16 +37,12 @@ jq --arg cert "$CERT_PATH" \
     .phish_server.key_path = $key |
     .contact_address = $email' \
     config.json > config_temp.json && mv config_temp.json config.json
-echo "File config.json modified"
+green "File config.json modified"
 sudo chmod +x gophish
-echo "Installing screen"
-sudo apt install screen
-echo "Starting screen called 'go_phish'"
-screen -S go_phish
-echo "Executing go_phish"
+#green "Installing screen"
+#sudo apt install screen
+#green "Starting screen called 'go_phish'"
+#screen -S go_phish
+green "Executing go_phish"
 sudo ./gophish
-
-echo "You can see all the screens with 'screen ls'"
-echo "To change screen you can click Cntrl+A, D"
-echo "To join gophish screen again, run 'screen -r go_phish'"
-echo "Done. return regards;"
+green "Done. return regards;"
