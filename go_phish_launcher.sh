ae254a87e76a3804589aa8ca69dc385348862920
vidar <shanlogauthier@gmail.com>
2025-05-07 15:50:14 +0200

    Add autolauncher

---
+++ go_phish_launcher.sh
@@ -0,0 +1,37 @@
+#remember to change permission of the file. 'chmod +x go_phish_launcher
+#and then launch the script 'sudo ./go_phish_launsher.sh'
+sudo echo "Starting..."
+echo "Installing go phish"
+cd ~ || exit
+mkdir "go_phish"
+echo "Created folder on " + pwd
+cd go_phish || exit
+wget https://github.com/gophish/gophish/releases/download/v0.12.1/gophish-v0.12.1-linux-64bit.zip
+sudo apt install unzip
+unzip gophish-v0.12.1-linux-64bit.zip
+sudo apt install openssl
+read -n1 -r -p "We will create a SSL certificate. The only required field is the email. You can click on 'enter' in the rest of fields" key
+openssl req -newkey rsa:2048 -nodes -keyout gophish.key -x509 -days 365 -out gophish.crt
+sudo apt install jq -y
+echo "Updating config.json..."
+jq '.admin_server.listen_url = "0.0.0.0:3333" |
+    .admin_server.cert_path = "/home/ubuntu/gophish.crt" |
+    .admin_server.key_path = "/home/ubuntu/gophish.key" |
+    .phish_server.cert_path = "/home/ubuntu/gophish.crt" |
+    .phish_server.key_path = "/home/ubuntu/gophish.key" |
+    .contact_address = "jdortolan@gmail.com"' \
+    config.json > config_temp.json && mv config_temp.json config.json
+
+echo "File config.json modified"
+sudo chmod +x gophish
+echo "Installing screen"
+sudo apt install screen
+echo "Starting screen called 'go_phish'"
+screen -S go_phish
+echo "Executing go_phish"
+sudo ./gophish
+
+echo "You can see all the screens with 'screen ls'"
+echo "To change screen you can click Cntrl+A, D"
+echo "To join gophish screen again, run 'screen-r go_phish'"
+echo "Done. return regards;"
